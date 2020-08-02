4550f248a4d82eb55f5308c79d8a3d63207f43ee
Mohamed Kari <contact@mkari.de>
2020-08-02 15:57:06 +0200

    refactor looking at launcher

--- toy_example.py
+++ toy_example.py
@@ -21,27 +21,22 @@ class ToyModel(nn.Module):
         return self.net2(self.relu(self.net1(x)))
 
 
-def demo_basic(local_world_size, gpu_rank):
-    print(
-        f"[{os.getpid()}] rank = {dist.get_rank()}, "
-        + f"world_size = {dist.get_world_size()}"
-    )
-
-    model = ToyModel().cuda(gpu_rank)
-    ddp_model = DistributedDataParallel(model, [gpu_rank])
+def train(local_rank):
+    model = ToyModel().cuda(local_rank)
+    ddp_model = DistributedDataParallel(model, [local_rank])
 
     loss_fn = nn.MSELoss()
     optimizer = optim.SGD(ddp_model.parameters(), lr=0.001)
 
     optimizer.zero_grad()
     outputs = ddp_model(torch.randn(20, 10))
-    labels = torch.randn(20, 5).to(gpu_rank)
+    labels = torch.randn(20, 5).to(local_rank)
     loss_fn(outputs, labels).backward()
     optimizer.step()
     print("my_parameters", list(model.parameters()))
 
 
-def spmd_main(local_world_size, gpu_rank):
+def setup(local_rank):
     # These are the parameters used to initialize the process group
     env_dict = {
         key: os.environ[key]
@@ -49,24 +44,24 @@ def spmd_main(local_world_size, gpu_rank):
     }
     print(f"[{os.getpid()}] Initializing process group with: {env_dict}")
     dist.init_process_group(backend="nccl")
+    
     print(
-        f"[{os.getpid()}]: world_size = {dist.get_world_size()}, "
-        + f"rank = {dist.get_rank()}, backend={dist.get_backend()}"
+        f"[PID {os.getpid()}]:"
+        f" world_size = {dist.get_world_size()}"
+        f" rank = {dist.get_rank()}"
+        f" backend={dist.get_backend()}"
     )
 
-    demo_basic(local_world_size, gpu_rank)
+    train(local_rank)
 
     # Tear down the process group
     dist.destroy_process_group()
 
 
 if __name__ == "__main__":
+    print(sys.argv, os.environ)
     parser = argparse.ArgumentParser()
-    # This is passed in via launch.py
     parser.add_argument("--local_rank", type=int, default=0)
-    # This needs to be explicitly passed in
-    parser.add_argument("--local_world_size", type=int, default=1)
     args = parser.parse_args()
-    print(args)
-    # The main entry point is called directly without using subprocess
-    spmd_main(args.local_world_size, args.local_rank)
+
+    setup(args.local_rank)
