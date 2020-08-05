724b320c8b7437208f714c92c7208ac84b132474
Mohamed Kari <contact@mkari.de>
2020-08-05 09:33:43 +0200

    dettach containers

--- imagenet-dist/run-imagenet-node-0.sh
+++ imagenet-dist/run-imagenet-node-0.sh
@@ -1,3 +1,3 @@
 eval $(docker-machine env mo) && \
     docker-compose -f docker-compose.imagenet-dist.yml build && \
-    docker-compose -f docker-compose.imagenet-dist.yml run --service-ports -e NODE_RANK=0 imagenet-dist
+    docker-compose -f docker-compose.imagenet-dist.yml run --detach --service-ports -e NODE_RANK=0 imagenet-dist
--- imagenet-dist/run-imagenet-node-1.sh
+++ imagenet-dist/run-imagenet-node-1.sh
@@ -1,3 +1,3 @@
 eval $(docker-machine env node-2-distributed-training) && \
     docker-compose -f docker-compose.imagenet-dist.yml build && \
-    docker-compose -f docker-compose.imagenet-dist.yml run --service-ports -e NODE_RANK=1 imagenet-dist
+    docker-compose -f docker-compose.imagenet-dist.yml run --detach --service-ports -e NODE_RANK=1 imagenet-dist
